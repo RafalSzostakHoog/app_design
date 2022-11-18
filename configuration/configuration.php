15f5bc02c5f9076db0e9a69b8ec7c99cc5e09616
RafalSzostakHoog <szostakrafal97x@gmail.com>
2022-11-18 15:14:16 +0100

    Step 3

---
+++ configuration/configuration.php
@@ -0,0 +1,15 @@
+<?php         
+	
+//Sekcja Definjicji	
+date_default_timezone_set('Europe/Warsaw');
+	
+DEFINE('TITLE_NAME', "VirtuGame"); 	//Nazwa Strony
+DEFINE('IMAGE_LOGO', "/image/logo.png"); 	//Nazwa Strony
+
+
+//Połączenie Bazdy Danych
+DEFINE('SQL_HOST', "localhost"); // IP serwera z mysql
+DEFINE('SQL_USER', "root"); // Login do navicata !
+DEFINE('SQL_PASS', "123"); // Haslo do navicata !
+DEFINE('SQL_DB', "baza"); // Nazwa Bazy Account !
+?>
